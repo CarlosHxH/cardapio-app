3d7f68f5eceb39d7223866e03cab7d5aa7766346
Carlos Alves <110472397+CarlosHxH@users.noreply.github.com>
2026-06-22 14:53:47 +0000

    feat: corrige estilo do componente Toaster removendo a borda

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Toaster toastOptions={{
           style: {
             background: 'none',
+            border: 'none',
           },
         }}/>
       </AuthProvider>
